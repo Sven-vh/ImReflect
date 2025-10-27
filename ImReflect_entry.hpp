a14ae15ab08408c33b8d1017677b7278608aa5f8
Sven <svanhuessen@gmail.com>
2025-10-27 18:58:34 +0100

    fix: example in comment

--- ImReflect_entry.hpp
+++ ImReflect_entry.hpp
@@ -4,7 +4,16 @@
 #include <extern/svh/tag_invoke.hpp>
 #include <extern/visit_struct/visit_struct.hpp>
 
-/* Define this for you class or struct */
+/*
+Define this underneath your class or struct with the appropriate member variables 
+Example:
+struct MyStruct {
+	int a;
+	float b;
+	std::string c;
+};
+IMGUI_REFLECT(MyStruct, a, b, c)
+*/
 #define IMGUI_REFLECT(T, ...) \
 VISITABLE_STRUCT_IN_CONTEXT(ImReflect::Detail::ImContext, T, __VA_ARGS__);
 
